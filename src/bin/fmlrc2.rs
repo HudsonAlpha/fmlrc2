421448e5ca3bdb3aa2776116cbb0575019b3b40e
holtjma <jholt@hudsonalpha.org>
2020-07-02 15:16:04 -0500

    forgot the sort

--- src/bin/fmlrc2.rs
+++ src/bin/fmlrc2.rs
@@ -100,6 +100,7 @@ fn main() {
         error!("--begin_index set to value larger than --end_index");
         std::process::exit(exitcode::DATAERR);
     }
+    kmer_sizes.sort();
     info!("\tk-mer sizes: {:?}", kmer_sizes);
     info!("\tabs. mininimum count: {}", min_count);
     info!("\tdyn. minimimum fraction: {}", min_frac);
