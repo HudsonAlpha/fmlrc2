d51c139154c31b2380cee74c6843419f511c7eac
holtjma <jholt@hudsonalpha.org>
2020-10-16 14:46:39 -0500

    adding/fixing docs

--- src/dynamic_wfa.rs
+++ src/dynamic_wfa.rs
@@ -1,14 +1,21 @@
 
+/// Data structure for doing dynamic edit-distance only WFA calculation against a fixed string
 #[derive(Clone,Debug,Eq,PartialEq)]
 pub struct DynamicWFA<'a> {
+    /// the fixed sequence to compare to
     primary_seq: &'a [u8],
+    /// the dynamic sequence that will grow over time
     other_seq: Vec<u8>,
+    /// current edit distance (starts at 0)
     edit_distance: usize,
+    /// current distance vector, will grow in length as ED grows
     distance_vector: Vec<usize>,
+    /// farther point along the fixed sequence so far
     max_distance: usize
 }
 
 impl<'a> Ord for DynamicWFA<'a> {
+    /// This ordering is by (max_distance / edit_distance); i.e. fewer edits over longer distances is "better"
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         (self.max_distance as f64 / (self.edit_distance+1) as f64).partial_cmp(
             &(other.max_distance as f64 / (other.edit_distance+1) as f64)
@@ -22,6 +29,16 @@ impl<'a> PartialOrd for DynamicWFA<'a> {
 }
 
 impl<'a> DynamicWFA<'a> {
+    /// Creates an instance of DynamicWFA with proper default settings
+    /// # Arguments
+    /// * `primary_seq` - a reference to the primary (fixed) sequence to compare against
+    /// # Examples
+    /// ```rust
+    /// use fmlrc::dynamic_wfa::DynamicWFA;
+    /// // AAGAT in u8 format
+    /// let fixed_seq: Vec<u8> = vec![1, 1, 3, 1, 5];
+    /// let mut wfa_compare = DynamicWFA::new(&fixed_seq);
+    /// ```
     #[inline]
     pub fn new(primary_seq: &'a [u8]) -> DynamicWFA<'a> {
         DynamicWFA {
@@ -33,21 +50,45 @@ impl<'a> DynamicWFA<'a> {
         }
     }
 
+    /// Gets the dynamic sequence so far
     #[inline]
     pub fn get_other_seq(&self) -> &Vec<u8> {
         &self.other_seq
     }
 
+    /// Gets the edit distance so far
     #[inline]
     pub fn get_edit_distance(&self) -> usize {
         self.edit_distance
     }
 
+    /// Gets the maximum distance into `primary_seq` (the fixed sequence) so far
     #[inline]
     pub fn get_max_distance(&self) -> usize {
         self.max_distance
     }
 
+    /// This is the primary function that needs to be called to do any comparisons.
+    /// It appends a symbol to the dynamic sequence and attempts to extend the diagonals.
+    /// If none of the diagonals extend, it means the only way forward is to increase the edit distance,
+    /// so it will do that and then re-extend from there.
+    /// Returns the current maximum distance and current edit distance as a tuple.
+    /// Note that once the end of the sequence is reached, current maximum distance may become greater 
+    /// that the length of the `primary_seq` in subsequent `append(...)`.
+    /// # Arguments
+    /// * `symbol` - a single character to add to the dynamic sequence
+    /// # Examples
+    /// ```rust
+    /// use fmlrc::dynamic_wfa::DynamicWFA;
+    /// // AAGAT in u8 format
+    /// let fixed_seq: Vec<u8> = vec![1, 1, 3, 1, 5];
+    /// let mut wfa_compare = DynamicWFA::new(&fixed_seq);
+    /// // compare to AGAT
+    /// assert_eq!(wfa_compare.append(1), (1, 0));
+    /// assert_eq!(wfa_compare.append(3), (3, 1));
+    /// assert_eq!(wfa_compare.append(1), (4, 1));
+    /// assert_eq!(wfa_compare.append(5), (5, 1));
+    /// ```
     #[inline]
     pub fn append(&mut self, symbol: u8) -> (usize, usize) {
         self.other_seq.push(symbol);
@@ -64,7 +105,20 @@ impl<'a> DynamicWFA<'a> {
         (self.max_distance, self.edit_distance)
     }
 
-
+    /// This is necessary when the dynamic sequence is finished but it has not yet reached the full length of the fixed sequence.
+    /// It will repeatedly increase the edit distance until the end is reachable.  If the dynamic sequence is shorter
+    /// than the fixed sequence, it may be necessary to call this to get a final edit distance.
+    /// # Examples
+    /// ```rust
+    /// use fmlrc::dynamic_wfa::DynamicWFA;
+    /// // AAGAT in u8 format
+    /// let fixed_seq: Vec<u8> = vec![1, 1, 3, 1, 5];
+    /// let mut wfa_compare = DynamicWFA::new(&fixed_seq);
+    /// // compare to AG (which won't reach the end)
+    /// assert_eq!(wfa_compare.append(1), (1, 0));
+    /// assert_eq!(wfa_compare.append(3), (3, 1));
+    /// assert_eq!(wfa_compare.finalize(), (5, 3));
+    /// ```
     #[inline]
     pub fn finalize(&mut self) -> (usize, usize) {
         while self.max_distance < self.primary_seq.len() {
@@ -77,6 +131,8 @@ impl<'a> DynamicWFA<'a> {
         (self.max_distance, self.edit_distance)
     }
 
+    /// Helper function that will extend all diagonals as far as it can.
+    /// Returns true as long as _something_ is extended.
     #[inline]
     fn maximal_extend(&mut self) -> bool {
         let mut extension_made = false;
@@ -92,6 +148,8 @@ impl<'a> DynamicWFA<'a> {
         extension_made
     }
 
+    /// Helper function that increases the edit distance and creates all the new "diagonals" based 
+    /// on that extension.
     #[inline]
     fn increase_edit_distance(&mut self) {
         //actually increase the edit distance
